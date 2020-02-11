8bcb9d6cc79f50bfb202c44dde4332a09183d921
Paul JULLIEN <paul.jullien.code@gmail.com>
2020-02-11 15:53:08 +0100

    Embauche de la fonction de distrib (ne fonctionne pas correctement)

--- src/paul/ft_distribution.c
+++ src/paul/ft_distribution.c
@@ -6,35 +6,37 @@
 /*   By: pauljull <pauljull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/20 08:59:33 by pauljull          #+#    #+#             */
-/*   Updated: 2020/01/30 13:10:26 by pauljull         ###   ########.fr       */
+/*   Updated: 2020/02/11 14:39:33 by pauljull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/lem_in.h"
 
 /*
+	Fonction qui calcule le nombre de fourmi à envoyer sur un chemin donné dans le cas général (sum % nb_path == 0)
+*/
+
+int		ft_process_distrib(t_path *path, t_map *data)
+{
+	return (((data->nb_ants + data->sum_len_path - data->nb_path) / data->nb_path) + 1 - path->len);
+}
+
+/*
 	Fonction qui va redistribuer les fourmis de maniere équitable.
 	entre les différents chemin en prenant en compte la taille des chemin.
 */
 
-void	ft_distribution(int **tab_path, int nb_path, int nb_ants)
+void	ft_distribution(t_path *path, t_map *data)
 {
-	int t;
-	int i;
+	t_path	*head;
+	int		verif_sum;
 
-	t = 0;
-	while (nb_ants > 0)
+	head = path;
+	verif_sum = 0;
+	while (path != NULL)
 	{
-		i = 0;
-		t += 1;
-		while (nb_ants > 0 && i < nb_path)
-		{
-			if (t >= tab_path[i][0])
-			{
-				tab_path[i][1] += 1;
-				nb_ants -= 1;
-			}
-			i += 1;
-		}
+		printf("len = %d -----------\n", path->len);
+		path = path->next;
 	}
+	printf("nb_ants = %d\nverif_sum = %d\nComplexite totale = %d\n", data->nb_ants, data->sum_len_path, (data->nb_ants + data->sum_len_path - data->nb_path) / data->nb_path);
 }
