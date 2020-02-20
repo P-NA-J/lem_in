0858b6310beea7359cefe0c7f7e111eb9d2fb9d4
Paul JULLIEN <paul.jullien.code@gmail.com>
2020-02-20 21:47:50 +0100

    bubble sort

--- src/paul/ft_bubble_sort.c
+++ src/paul/ft_bubble_sort.c
@@ -6,41 +6,48 @@
 /*   By: pauljull <pauljull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 17:55:34 by pauljull          #+#    #+#             */
-/*   Updated: 2020/02/14 17:56:17 by pauljull         ###   ########.fr       */
+/*   Updated: 2020/02/20 21:28:53 by pauljull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/lem_in.h"
 
-void	ft_bubble_sort(int *tab_l, int *tab_i, int len)
+static void	ft_swap_int(int tab[MAX_LEN_PATH], int i, int j)
 {
-	int i;
-	int j;
-	int	i_min;
-	int	min;
 	int	tmp;
 
-	i_min = 0;
+	tmp = tab[i];
+	tab[i] = tab[j];
+	tab[j] = tmp;
+}
+
+static void	ft_sort_max(t_opti *opti, int len)
+{
+	int	i;
+	int	max;
+	int	i_max;
+
+	max = opti->len_p[0][0];
+	i_max = 0;
 	i = 0;
 	while (i < len)
 	{
-		min = tab_l[i];
-		i_min = i;
-		j = i + 1;
-		while (j < len)
+		if (opti->len_p[0][i] > max)
 		{
-			if (tab_l[j] < min)
-			{
-				min = tab_l[j];
-				i_min = j;
-			}
-			j += 1;
+			max = opti->len_p[0][i];
+			i_max = i;
 		}
-		tab_l[i_min] = tab_l[i];
-		tmp = tab_i[i_min];
-		tab_i[i_min] = tab_i[i];
-		tab_l[i] = min;
-		tab_i[i] = tmp;
 		i += 1;
+	}
+	ft_swap_int(opti->len_p[0], i - 1, i_max);
+	ft_swap_int(opti->index_p[0], i - 1, i_max);
+}
+
+void		ft_bubble_sort(t_opti *opti, int len)
+{
+	while (len > 0)
+	{
+		ft_sort_max(opti, len);
+		len -= 1;
 	}
 }
